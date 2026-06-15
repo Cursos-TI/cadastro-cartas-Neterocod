79a9730d5bd2c61da3688a68255bb8ba1c95a106
Neterocod <mendesdaiane@live.com>
2026-06-15 00:07:49 +0000

    int char: estado printf ("digite uma letra de 'a' a 'h'") scanf %c : estados printf area de exibição da letra do estado.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,20 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+
+  char estados
+
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
+printf ("digite Uma letra de 'A' a 'H representando os 8 estados: \n");
+scanf("%c", estados")
 
   // Área para exibição dos dados da cidade
+printf("estados: "%c")\n;
+
+
 
 return 0;
 } 
